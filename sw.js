201fce000dca60ed0d7670afae7f5b3dcb83238c
baybilon <baybilon_iqbal@hotmail.com>
2021-01-03 19:04:36 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2742a2ea81adbcd2e1d4.js"
+    "url": "webpack-runtime-669288db28327ff990d6.js"
   },
   {
     "url": "framework-8e528b732ab2eaadb7b7.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8ed316d523f5c617a14ae37d023eb20f"
+    "revision": "0187dd38c82b85ca7afad11910c3f0e4"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "94626f9e583dc1af7d68d766f480a32f"
+    "revision": "5258c86472815bc97bf6fb0f96a6ca55"
   },
   {
     "url": "polyfill-78cdc6a84a6e659c51c0.js"
