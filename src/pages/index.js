17a1e5ce9f4d0987847218258f5ee506080f0f73
baybilon <baybilon_iqbal@hotmail.com>
2021-03-23 21:22:56 +0700

    Add some img

--- src/pages/index.js
+++ src/pages/index.js
@@ -101,6 +101,13 @@ const img_set_4 = [
     desc: 'Wedding event',
     full: true,
   },
+  {
+    src: require('../assets/images/gallery/fulls/photo/aloe.jpg'),
+    thumbnail: require('../assets/images/gallery/thumbs/photo/aloe.jpg'),
+    title: 'Freelance',
+    desc: 'Photo Product',
+    full: true,
+  },
 ];
 
 const IndexPage = () => (
